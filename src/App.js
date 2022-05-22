4391ee1061bbfc62d4fd32eee9a130723a226452
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-05-22 22:31:47 +0600

    firebase add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./Shared/Navbar";
 
 function App() {
+  const [theme, setTheme] = useState(null);
+
   return (
-    <div className="">
-      <Navbar></Navbar>
+    <div className="" data-theme={!theme && "cupcake"}>
+      <Navbar setTheme={setTheme} theme={theme}></Navbar>
     </div>
   );
 }
